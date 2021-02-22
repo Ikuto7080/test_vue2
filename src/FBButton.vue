cab5151ae6f9abd142f8fe96c21588ce8e55a63c
Ikuto Hongu <ikuto.hongu@gmail.com>
2021-02-22 23:20:27 +0900

    change urls to heroku version

--- src/FBButton.vue
+++ src/FBButton.vue
@@ -15,7 +15,7 @@ export default {
     name: 'FBButton',
     methods: {
         open() {
-            document.location.href = 'https://www.facebook.com/v9.0/dialog/oauth?client_id=420945845838455&redirect_uri=https://localhost:8080/fb_user_info/'
+            document.location.href = 'https://www.facebook.com/v9.0/dialog/oauth?client_id=420945845838455&redirect_uri=https://obscure-reef-20222.herokuapp.com/fb_user_info/'
         }
    }
 }
