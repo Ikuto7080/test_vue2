6cf26a8783715b3b65633885f451b00e1c7916e4
Ikuto Hongu <ikuto.hongu@gmail.com>
2021-02-22 23:27:12 +0900

    add express

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+var history = require('connect-history-api-fallback');
+
+
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
+app.use(history());
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
