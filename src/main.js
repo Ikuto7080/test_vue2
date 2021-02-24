8728cd2fe32423f1cdb1f3ddf2e3503c0eace163
Ikuto Hongu <ikuto.hongu@gmail.com>
2021-02-25 08:30:32 +0900

    add vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -16,5 +17,6 @@ if (account) {
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
